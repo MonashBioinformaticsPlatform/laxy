1fb60be3dbee46e6f1dc72796ba160cce5fa786e
Andrew Perry <ajperry@pansapiens.com>
2018-01-19 14:19:19 +1100

    Add basic admin views for models.

--- laxy_backend/admin.py
+++ laxy_backend/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from reversion.admin import VersionAdmin
 
-# Register your models here.
+from .models import Job, ComputeResource, File, FileSet
+
+
+class ComputeResourceAdmin(VersionAdmin):
+    pass
+
+
+class JobAdmin(VersionAdmin):
+    pass
+
+
+class FileAdmin(VersionAdmin):
+    pass
+
+
+class FileSetAdmin(VersionAdmin):
+    pass
+
+
+# admin.site.register(TaskMeta, TaskMetaAdmin)
+admin.site.register(Job, JobAdmin)
+admin.site.register(ComputeResource, ComputeResourceAdmin)
+admin.site.register(File, FileAdmin)
+admin.site.register(FileSet, FileSetAdmin)
