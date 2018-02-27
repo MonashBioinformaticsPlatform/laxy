dd7514a75fc08daa760f4a5f7a422c5e31e6f84f
Andrew Perry <ajperry@pansapiens.com>
2018-02-27 13:21:01 +1100

    Enable django-extensions.

--- laxy/default_settings.py
+++ laxy/default_settings.py
@@ -117,7 +117,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'django_extensions',
+    'django_extensions',
     'django_celery_results',
     'django_celery_beat',
     'rest_framework',
