58851372190b50b38b30c6077e599bb47f8e8402
Andrew Perry <ajperry@pansapiens.com>
2018-05-01 16:02:23 +1000

    Grab iGenomes links in JSON format.

---
+++ scripts/get_igenomes_json.groovy
@@ -0,0 +1,34 @@
+#!/usr/bin/env groovy
+
+/*
+Download a Groovy config (NextFlow) from SciLifeLab/NGI-RNASeq
+and output a JSON version
+
+$ groovy scripts/get_igenomes_json.groovy >laxy_backend/templates/genomes.json
+*/
+
+import groovy.json.JsonOutput
+
+/*
+public String get_url_content(url) {
+
+    def urlContents = url.toURL().getText()
+
+    return urlContents
+}
+
+text = get_url_content("https://raw.githubusercontent.com/SciLifeLab/NGI-RNAseq/master/conf/igenomes.config")
+text.eachLine {
+    println it
+}
+*/
+
+// final ConfigObject params = new ConfigSlurper().parse(new File("params.groovy").toURI().toURL());
+
+def params = [params: [igenomes_base:'ftp://igenome:G3nom3s4u@ussd-ftp.illumina.com']]
+def slurper = new ConfigSlurper()
+slurper.setBinding(params)
+final ConfigObject config = slurper.parse("https://raw.githubusercontent.com/SciLifeLab/NGI-RNAseq/master/conf/igenomes.config".toURI().toURL())
+// println config.toString()
+
+println JsonOutput.prettyPrint(JsonOutput.toJson(config)).stripIndent()
