c84b1c2587a9b71f3ccc4c28ef60bb5ea98d3ac1
Andrew Perry <ajperry@pansapiens.com>
2019-01-25 16:19:49 +1100

    Suppress warning related to mutating snackbar prop.

--- laxy_frontend/src/snackbar.ts
+++ laxy_frontend/src/snackbar.ts
@@ -5,7 +5,7 @@ export class Snackbar {
     public static component: any;
 
     public static flashMessage(message: string, duration: number = 2000) {
-        Snackbar.component.mdDuration = duration;
+        // Snackbar.component.mdDuration = duration;  // shouldn't change props this way
         Store.commit(SET_GLOBAL_SNACKBAR, {message: message, duration: duration});
         Snackbar.component.open();
     }
