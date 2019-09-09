e602040750adb7073c17104cdfc8982aa6f5ae3e
Andrew Perry <ajperry@pansapiens.com>
2019-09-09 16:27:20 +1000

    Add some remote debugging code (commented).

--- manage.py
+++ manage.py
@@ -20,3 +20,17 @@ if __name__ == "__main__":
             )
         raise
     execute_from_command_line(sys.argv)
+
+    # Start the remote debugger for PyCharm if in debug mode
+    # You must pip install the exact version of pydevd-pycharm matching your IDE version
+    # eg:  pip install pydevd-pycharm~=192.6262.63
+    #
+    # from django.conf import settings
+    # if settings.DEBUG and sys.argv[1] == 'runserver':
+    #     import pydevd_pycharm
+    #     debug_host = 'host.docker.internal'
+    #     pydevd_pycharm.settrace(debug_host, port=21001,
+    #                             suspend=False,
+    #                             stdoutToServer=True, stderrToServer=True)
+    #     print("Initialized debugger client that will connect to: ", debug_host)
+    #     print("Insert code the `import pydevd_pycharm; pydevd_pycharm.settrace()` to trigger the debugger.")
