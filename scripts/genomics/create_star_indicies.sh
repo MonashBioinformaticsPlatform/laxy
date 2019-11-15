c7fe67a0479553f8c4976d2b7727ee70368855c4
Andrew Perry <ajperry@pansapiens.com>
2019-11-15 12:12:54 +1100

    Add Plasmodium falciparum to the STAR index helper script.

--- scripts/genomics/create_star_indicies.sh
+++ scripts/genomics/create_star_indicies.sh
@@ -36,20 +36,22 @@ make_index Homo_sapiens/Ensembl/GRCh38
 make_index Homo_sapiens/UCSC/hg19
 make_index Homo_sapiens/UCSC/hg38
 
-make_index Saccharomyces_cerevisiae/Ensembl/R64-1-1     4  16G 1 "--genomeSAindexNbases 10"
-make_index Caenorhabditis_elegans/Ensembl/WBcel235      8  32G 1 "--genomeSAindexNbases 12"
-make_index Escherichia_coli_K_12_DH10B/Ensembl/EB1      4  16G 1 "--genomeSAindexNbases 10"
+make_index Saccharomyces_cerevisiae/Ensembl/R64-1-1       4  16G 1 "--genomeSAindexNbases 10"
+make_index Caenorhabditis_elegans/Ensembl/WBcel235        8  32G 1 "--genomeSAindexNbases 12"
+make_index Escherichia_coli_K_12_DH10B/Ensembl/EB1        4  16G 1 "--genomeSAindexNbases 10"
 
-make_index Mus_musculus/Ensembl/GRCm38                 24  128G 3
-make_index Mus_musculus/NCBI/GRCm38                    24  128G 3
+make_index Mus_musculus/Ensembl/GRCm38                   24  128G 3
+make_index Mus_musculus/NCBI/GRCm38                      24  128G 3
 
-make_index Acinetobacter_baumannii/Custom/AB307-0294    4  16G 1 "--genomeSAindexNbases 9"
-make_index Acinetobacter_baumannii/Custom/ATCC19606     4  16G 1 "--genomeSAindexNbases 10"
+make_index Acinetobacter_baumannii/Custom/AB307-0294      4  16G 1 "--genomeSAindexNbases 9"
+make_index Acinetobacter_baumannii/Custom/ATCC19606       4  16G 1 "--genomeSAindexNbases 10"
 
-make_index Aedes_aegypti/VectorBase/AaegL5.2           24  128G 3
-make_index Aedes_aegypti/NCBI/GCF_002204515.2_AaegL5.0 24  128G 3
+make_index Aedes_aegypti/VectorBase/AaegL5.2             24  128G 3
+make_index Aedes_aegypti/NCBI/GCF_002204515.2_AaegL5.0   24  128G 3
 
-make_index Danio_rerio/Ensembl/GRCz11.97               24 192G 3 "--limitGenomeGenerateRAM 151278166400"
-make_index Danio_rerio/Ensembl/GRCz11.97-noalt         24 192G 3 "--limitGenomeGenerateRAM 151278166400"
+make_index Danio_rerio/Ensembl/GRCz11.97                 24 192G 3 "--limitGenomeGenerateRAM 151278166400"
+make_index Danio_rerio/Ensembl/GRCz11.97-noalt           24 192G 3 "--limitGenomeGenerateRAM 151278166400"
 
-make_index Chelonia_mydas/NCBI/CheMyd_1.0-exon-contigs 16  64G 3 "--genomeChrBinNbits=14"
+make_index Chelonia_mydas/NCBI/CheMyd_1.0-exon-contigs   16  64G 3 "--genomeChrBinNbits=14"
+
+make_index Plasmodium_falciparum/PlasmoDB/3D7-release-39  4 16G 1 "--genomeSAindexNbases 11"
