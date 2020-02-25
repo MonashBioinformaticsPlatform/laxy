b6496bc1d2311eeb4782a99652ca708030e8da8a
Andrew Perry <ajperry@pansapiens.com>
2020-02-25 17:01:05 +1100

    Change Let's Encrypt client to simple_le.
    kristapsdz/acme-client became deprecated on Linux.
    
    Also adds anacron and a wait loop to the nginx compose config to try and make renewel and initial startup more reliable.

--- docker/ssl-certs/update-ssl-cert.sh
+++ docker/ssl-certs/update-ssl-cert.sh
@@ -1,33 +1,42 @@
-#!/bin/sh
-# /etc/periodic/daily/update-ssl-cert.sh
-# Must be: chmod +x /etc/periodic/daily/update-ssl-cert.sh
+#!/bin/bash
+# /etc/cron.daily/update-ssl-cert.sh
+# Must be: chmod +x /etc/cron.daily/update-ssl-cert.sh
 
 ACME_SSL_DOMAINS="${ACME_SSL_DOMAINS-laxy.io api.laxy.io dev.laxy.io dev-api.laxy.io}"
 
-# acme-client docs: https://kristaps.bsd.lv/acme-client/acme-client.1.html
-# EXTRA_ARGS="-s -F" # -s = staging, -F = force renewal
-ACME_EXTRA_ARGS="${ACME_EXTRA_ARGS- }" # -s = staging, -F = force renewal
-AGREEMENT_URL="https://letsencrypt.org/documents/LE-SA-v1.2-November-15-2017.pdf"
-
-mkdir -p /usr/share/nginx/html/.well-known/acme-challenge
-rm -rf /var/log/ssl-certs-cron.log /var/log/ssl-certs-cron.err
-
-# If there is no key, nginx won't start, so create a temporary self-signed one. acme-client will replace it.
-if [ ! -f /certs/domain.key ]; then
-  openssl req -x509 -newkey rsa:4096 \
-              -keyout /certs/domain.key \
-              -out /certs/fullchain.pem \
-              -days 1 -nodes -subj '/CN=localhost' \
-              >>/var/log/ssl-certs-cron.log 2>>/var/log/ssl-certs-cron.err
+EMAIL_OPT=""
+if [[ ! -z ${LAXY_ADMIN_EMAIL} ]]; then
+    EMAIL_OPT="--email ${LAXY_ADMIN_EMAIL}"
 fi
 
-/usr/bin/acme-client \
-            -a $AGREEMENT_URL \
-            -f /certs/account.key \
-            -C /usr/share/nginx/html/.well-known/acme-challenge/ \
-            -c /certs \
-            -k /certs/domain.key \
-            -Nnmev \
-            $ACME_EXTRA_ARGS \
-            $ACME_SSL_DOMAINS \
-            >>/var/log/ssl-certs-cron.log 2>>/var/log/ssl-certs-cron.err
+mkdir -p /usr/share/nginx/html/.well-known/acme-challenge
+rm -rf /var/log/ssl-certs-cron.log
+touch /var/log/ssl-certs-cron.log
+
+DOMAIN_ARGS=$(echo ' '${ACME_SSL_DOMAINS} | sed "s/ / -d /g")
+
+function generate_selfsigned() {
+    if [[ ! -f /certs/key.pem ]] && [[ ! -f /certs/fullchain.pem ]]; then
+      openssl req -x509 -newkey rsa:4096 \
+                  -keyout /certs/key.pem  \
+                  -out /certs/fullchain.pem \
+                  -days 1 -nodes -subj '/CN=localhost' \
+                   >>/var/log/ssl-certs-cron.log 2>&1
+    fi
+}
+
+cd /certs/
+/usr/local/bin/simp_le \
+    -f account_key.json \
+    -f account_reg.json \
+    -f key.pem \
+    -f cert.pem \
+    -f fullchain.pem \
+    -f chain.pem \
+    ${DOMAIN_ARGS} \
+    ${EMAIL_ADDRESS} \
+    --default_root /usr/share/nginx/html \
+    >>/var/log/ssl-certs-cron.log 2>&1 || generate_selfsigned
+
+# If there is no key and fullchain nginx won't start. This might happen if the ACME client fails for some reason.
+# In that case, so create a temporary self-signed one (generate_selfsigned)
