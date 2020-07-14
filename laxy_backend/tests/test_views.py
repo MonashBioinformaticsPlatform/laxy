d1dc84b1528b36d31d24cbd08c2c28a0cc573543
Andrew Perry <ajperry@pansapiens.com>
2020-07-14 08:26:37 +0000

    Fix test.

--- laxy_backend/tests/test_views.py
+++ laxy_backend/tests/test_views.py
@@ -680,7 +680,7 @@ class JobViewTest(TestCase):
         )
 
         self.assertEqual(
-            with_sane_names["samples"][0]["name"],
+            with_sane_names["samples"][0]["sanitized_name"],
             "Im_an_ugly_2_L002_1_sample-NAME__r1_",
         )
 
