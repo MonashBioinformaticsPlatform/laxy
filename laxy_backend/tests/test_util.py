30ebff2519e9ae3dc2136a94c23b47e82dfed1f2
Andrew Perry <ajperry@pansapiens.com>
2021-12-20 01:46:24 +0000

    Modify test to reflect new filename sanitization behaviour.

--- laxy_backend/tests/test_util.py
+++ laxy_backend/tests/test_util.py
@@ -8,22 +8,22 @@ class UtilFunctionsTest(TestCase):
         pass
 
     def test_sanitize_filename(self):
-        horrid_sample_name = "my sàmplé ❤😸蛇影 (A/B; C), #1.txt.gz"
+        funky_sample_name = "my sàmplé ❤😸蛇影 (A/B; C), #1.txt.gz"
 
         self.assertEqual(
-            sanitize_filename(horrid_sample_name, unicode_to_ascii=False),
+            sanitize_filename(funky_sample_name, unicode_to_ascii=False),
             "my_sample__AB_C_1.txt.gz",
         )
 
         self.assertEqual(
-            sanitize_filename(horrid_sample_name, unicode_to_ascii=True),
-            "my_sample_She_Ying__AB_C_1.txt.gz",
+            sanitize_filename(funky_sample_name, unicode_to_ascii=True),
+            "my_sample_She_Ying_AB_C_1.txt.gz",
         )
 
         including_brackets = "-_.() %s%s" % (string.ascii_letters, string.digits)
         self.assertEqual(
             sanitize_filename(
-                horrid_sample_name,
+                funky_sample_name,
                 valid_filename_chars=including_brackets,
                 unicode_to_ascii=True,
             ),
