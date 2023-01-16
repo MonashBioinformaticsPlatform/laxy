620a9b13c1adbf056917e907ad8bf1ac20aa4fdd
Andrew Perry <ajperry@pansapiens.com>
2023-01-16 03:09:47 +0000

    NglViewer: Color structure by b-factors (pLDDT confidence for Alphafold/Openfold)

--- laxy_frontend/src/components/pipelines/openfold/ui/NglViewer.vue
+++ laxy_frontend/src/components/pipelines/openfold/ui/NglViewer.vue
@@ -43,7 +43,15 @@ export default class NglViewer extends Vue {
     const pdbResponse = await WebAPI.getBlobByUrl(url);
     const pdbBlob = pdbResponse.data;
     const stage = new NGL.Stage(this.viewportId);
-    stage.loadFile(pdbBlob, { ext: "pdb", defaultRepresentation: true });
+    const structure = await stage.loadFile(pdbBlob, { ext: "pdb", defaultRepresentation: true });
+    // TODO: The representation should be passed in as a @Prop, overriding the default
+    // TODO: Show a key for the color scale in the md-card on the LaxyNglViewer component
+    // Color by B-factor value ranges (Alphafold/Openfold put the pLDDT score in this field)
+    structure.addRepresentation('cartoon', {
+      colorDomain: [0, 49.999, 50, 69.999, 70, 89.999, 90, 100],
+      colorScale: ['#ff7d45', '#ff7d45', '#ffdb13', '#ffdb13', '#65cbf3', '#65cbf3', '#0053d6', '#0053d6'],
+      colorScheme: 'bfactor'
+    })
   }
 }
 </script>
