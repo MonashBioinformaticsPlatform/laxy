2f9680dbbf3970d40927ee5bb1e5265dbdbf0db9
Andrew Perry <ajperry@pansapiens.com>
2024-06-27 04:50:50 +0000

    Make default password hasher Argon2

--- laxy/default_settings.py
+++ laxy/default_settings.py
@@ -706,6 +706,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Requires argon2-cffi and bcrypt packages
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
